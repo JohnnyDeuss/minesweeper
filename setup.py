fe4accb82efd29d4df0cc4acd035b6fbd7931c34
git@github.com <johnny@deuss.xyz>
2018-11-18 05:19:15 +0100

    Switched to setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='minesweeper',
       version='1.0',
@@ -6,4 +6,7 @@ setup(name='minesweeper',
       author='Johnny Deuss',
       author_email='johnnydeuss@gmail.com',
       url='https://github.com/JohnnyDeuss/minesweeper',
+      project_urls={'Source': 'https://github.com/pypa/sampleproject/'},
+      install_requires=['PyQt5'],
+      packages=find_packages()
     )
