9ec542df70ed4a29f39606d9fe1f7084ea10bcf5
git@github.com <johnny@deuss.xyz>
2018-11-18 04:00:52 +0100

    Add setup.py

---
+++ minesweeper/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='minesweeper',
+      version='1.0',
+      description='Minesweeper game',
+      author='Johnny Deuss',
+      author_email='johnnydeuss@gmail.com',
+      url='https://github.com/JohnnyDeuss/minesweeper',
+    )
