f066558d212ba115a84a2f4ded12fe235b2182b8
git@github.com <johnny@deuss.xyz>
2018-11-25 08:58:28 +0100

    Correct setup.py project source URL

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
+
 setup(name='minesweeper',
       version='1.0',
       description='Minesweeper game',
       author='Johnny Deuss',
       author_email='johnnydeuss@gmail.com',
       url='https://github.com/JohnnyDeuss/minesweeper',
-      project_urls={'Source': 'https://github.com/pypa/sampleproject/'},
+      project_urls={'Source': 'https://github.com/JohnnyDeuss/minesweeper'},
       install_requires=['PyQt5'],
       packages=find_packages()
     )
